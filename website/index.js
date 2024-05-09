cb8056afd2b397b6c1be08c81b13830fb6ff05ac
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-05-08 21:39:32 -0700

    variables in JS

--- website/index.js
+++ website/index.js
@@ -6,5 +6,21 @@
 
 // this is a comment
 
-document.getElementById("myH1").textContent = 'Hello'
-document.getElementById("myP").textContent = "!"
+//document.getElementById("myH1").textContent = 'Hello'
+//document.getElementById("myP").textContent = "!"
+
+/*
+    variable = A container that stores a value
+               Behaves as if it were the value it contains.
+    
+    1. declaration  let x;
+    2. assigment    x = 100
+*/
+
+let fullName = "Nelson Jimenez";
+let age = 25;
+let student = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `Your age is ${age}`;
+document.getElementById("p3").textContent = `You are a student ${student}`;
