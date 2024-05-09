676df36cfc75b8bd97f16823514bb5881377ac3a
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-05-08 21:15:28 -0700

    get element example

--- website/index.js
+++ website/index.js
@@ -1,7 +1,10 @@
-console.log(`Hello`);
-console.log(`!`)
+//console.log(`Hello`);
+//console.log(`!`)
 
-window.alert(`This is an alert`)
-window.alert(`!`)
+//window.alert(`This is an alert`)
+//window.alert(`!`)
 
 // this is a comment
+
+document.getElementById("myH1").textContent = 'Hello'
+document.getElementById("myP").textContent = "!"
