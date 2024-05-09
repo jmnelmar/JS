9227a2cddae7d6c44a0050c7faca9ed3e261d562
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-05-09 02:14:55 -0700

    ternary operators

---
+++ website/ternary_operator/script.js
@@ -0,0 +1,17 @@
+// ternary operator =   a shortcut to if{} and else{} statements
+//                      helps to assign a variable based on a condition
+//                      condition ? codeIfTrue : codeIfFalse;
+
+//let time = 9;
+//let greeting = time < 12 ? "Good Morning" : "Good afternoon!";
+//console.log(greeting);
+
+let isStudent;
+
+let message = isStudent ? "You are a student" : "You are not a student";
+console.log(message);
+
+let purchaseAmount = 125;
+
+let discount = purchaseAmount >= 100 ? 10 : 0;
+console.log(`Your total is $${purchaseAmount - purchaseAmount*discount/100}`);
