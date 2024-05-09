56fc2c706021530a21998e6cce08bb726e07b23f
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-05-09 02:25:21 -0700

    switches

---
+++ website/switch/script.js
@@ -0,0 +1,29 @@
+// SWITCH = cant be an efficient replacement to many else if statements
+
+let day = 1
+
+switch(day){
+    case 1:
+        console.log("It is Monday");
+        break;
+    case 2:
+        console.log("It is Tuesday");
+        break;
+    case 3:
+        console.log("It is Wednesday");
+        break;
+    case 4:
+        console.log("It is Thuesday");
+        break;
+    case 5:
+        console.log("It is Friday");
+        break;
+    case 6:
+        console.log("It is Saturday");
+        break;
+    case 7:
+        console.log("It is Sunday");
+        break;
+    default:
+        console.log(`${day} is not a day`);
+}
