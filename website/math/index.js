25cbdf2ac14947488a9f410850cf1ec417090df7
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-05-08 23:32:38 -0700

    Math built in functions

---
+++ website/math/index.js
@@ -0,0 +1,23 @@
+/*
+Math = build-in object that provides a collection of properties and methods
+
+let x = -3.21;
+let y = 2;
+let z = 0;
+
+Math.round(x);
+Math.floor(x);
+Math.ceil(x);
+Math.trunc(x);
+Math.pow(x);
+Math.sqrt(x);
+Math.log(x);
+Math.sign(x);
+Math.cos(x);
+Math.tan(x);
+Math.abs(x);
+Math.sign(x);
+Math.max(x,y,z);
+Math.min(x,y,z);
+
+*/
