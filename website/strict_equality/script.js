af33f5dc007489c65f35e61f3308c4f76ec37c5f
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-05-13 21:39:21 -0700

    strict

---
+++ website/strict_equality/script.js
@@ -0,0 +1,31 @@
+// = assigmnent operator
+// == comparison operator (compare if values are equal)
+// === strict equality operator (compare if values  & datatye are equal)
+// != inequality operator
+// !== strict inequality operator
+
+const PI = 3.14;
+
+if (PI == "3.14"){
+    console.log("that is pi");
+}else{
+    console.log("not pi");
+}
+
+if (PI === "3.14"){
+    console.log("that is pi");
+}else{
+    console.log("not pi");
+}
+
+if (PI != "3.14"){
+    console.log("that is pi");
+}else{
+    console.log("not pi");
+}
+
+if (PI !== "3.14"){
+    console.log("that is pi");
+}else{
+    console.log("not pi");
+}
