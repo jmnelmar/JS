b87c554de3f31092d60fc9fc55f4f0e0db3564c4
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-05-13 21:28:55 -0700

    slicing

---
+++ website/method_chaining/script.js
@@ -0,0 +1,22 @@
+/*
+Method Chaining = calling one method after another
+                    in one continuos line of code
+*/
+
+// No methods chaining
+
+let username = window.prompt("Enter your username: ");
+//username = username.trim()
+//let letter = username.charAt(0);
+//letter = letter.toUpperCase();
+
+//let extraChars = username.slice(1);
+//extraChars = extraChars.toLowerCase();
+//username = letter + extraChars;
+
+//console.log(username);
+
+// Method chaining
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+console.log(username)
